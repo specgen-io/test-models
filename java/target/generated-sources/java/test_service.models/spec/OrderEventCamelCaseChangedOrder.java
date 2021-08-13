1911bd39a57c2eb1acf196fd718b2abb76d16594
Anastasiya <nislovskaya96@gmail.com>
2021-08-13 22:00:32 +0300

    maven-plugin: Changed pom configuration

---
+++ java/target/generated-sources/java/test_service.models/spec/OrderEventCamelCaseChangedOrder.java
@@ -0,0 +1,20 @@
+package test_service.models.spec;
+
+import java.time.*;
+import java.util.*;
+import java.math.BigDecimal;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonSubTypes.*;
+
+public class OrderEventCamelCaseChangedOrder implements OrderEventCamelCase {
+	@JsonUnwrapped
+	public OrderChanged data;
+
+	public OrderEventCamelCaseChangedOrder() {
+	}
+
+	public OrderEventCamelCaseChangedOrder(OrderChanged data) {
+		this.data = data;
+	}
+}
