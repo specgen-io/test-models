925893f1569d28f70d014c8505ec85759e2e7bee
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-11-21 00:50:43 -0500

    added kotlin-jackson tests (#20)
    * kotlin-jackson: Added initial version
    
    * Added kotlin-jackson job
    
    * Upgraded specgen
    
    * kotlin-jackson: Running unit tests
    
    * kotlin-jackson: Fixing unit tests
    
    * kotlin-jackson: Trying to fix tests
    
    * kotlin-jackson: Debugging tests results
    
    * kotlin-jackson: Added tests
    
    * kotlin-jackson: Trying get tests results from gradle project
    
    * kotlin-jackson: Removed extra step
    
    * Added gitignore; Removed README

---
+++ kotlin-jackson/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("io.specgen.gradle")
+}
+
+group = "io.specgen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+    testImplementation(kotlin("test"))
+}
+
+specgen {
+    modelsKotlin {
+        specFile.set(file("../spec.yaml"))
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
